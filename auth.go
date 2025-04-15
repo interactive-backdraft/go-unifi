08de346608b66bd1f442eb927e376fa2e80144a7
Matthew Sheeley <matthew.sheeley@gmail.com>
2025-04-14 21:53:02 -0400

    WIP

--- auth.go
+++ auth.go
@@ -1,6 +1,22 @@
 package unifi
 
+import (
+	"net/http"
+)
+
 func (c Client) Login() (bool, error) {
+
+	req, err := http.NewRequest("POST", c.baseURL+"/api/login", nil)
+	if err != nil {
+		return false, err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	if resp.StatusCode == 200 {
+		return true, nil
+	}
 	return false, nil
 }
 
